80349cefb5fff8b7df443d9aa4a10435043896f4
mykhailo.dubovskyi <mdub@ciklum.com>
2018-02-09 11:05:43 +0200

    -first commit

--- src/main/java/com/dubovskyi/films/Application.java
+++ src/main/java/com/dubovskyi/films/Application.java
@@ -1,5 +1,14 @@
 package com.dubovskyi.films;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application
 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
